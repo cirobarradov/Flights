4e12fdb10a18ba449f3e9ae093b46e5040cce614
cirobarradov <rbravo79@gmail.com>
2016-01-14 17:06:37 +0100

    test

--- src/main/scala/com/stratio/model/spark/FlightDsl.scala
+++ src/main/scala/com/stratio/model/spark/FlightDsl.scala
@@ -1,62 +1,108 @@
 package com.stratio.model.spark
 
-import com.stratio.model.{Flight, FuelPrice}
+import java.util.Calendar
+
+import com.stratio.model.{Delays, Cancelled, Flight, FuelPrice}
 import org.apache.spark.rdd.RDD
+import org.joda.time.LocalDate
 
 import scala.language.implicitConversions
 
 class FlightCsvReader(self: RDD[String]) {
 
+  //val depTime=4
   /**
-   *
-   * Parsea el RDD[String] de CSV's a un RDD[Flight]
-   *
-   * Tip: Usar método flatmap.
-   *
-   */
+    *
+    * Parsea el RDD[String] de CSV's a un RDD[Flight]
+    *
+    * Tip: Usar método flatmap.
+    *
+    */
   def toFlight: RDD[Flight] = {
-    ???
+    val c = self.map(x=>Flight( new LocalDate(
+      x.split(",")(0).toInt,x.split(",")(1).toInt+1, x.split(",")(2).toInt).toDate,
+      x.split(",")(4).toInt,
+      x.split(",")(5).toInt,
+      x.split(",")(6).toInt,
+      x.split(",")(7).toInt,
+      x.split(",")(8).toString,
+      x.split(",")(9).toInt,
+      x.split(",")(11).toInt,
+      x.split(",")(12).toInt,
+      x.split(",")(14).toInt,
+      x.split(",")(15).toInt,
+      x.split(",")(16).toString,
+      x.split(",")(17).toString,
+      x.split(",")(18).toInt,
+      Flight.parseCancelled(x.split(",")(21)),
+      x.split(",")(22).toInt,
+      Delays(Flight.parseCancelled(x.split(",")(24)),
+        Flight.parseCancelled(x.split(",")(25)),
+        Flight.parseCancelled(x.split(",")(26)),
+        Flight.parseCancelled(x.split(",")(27)),
+        Flight.parseCancelled(x.split(",")(28)))))
+    c
+
   }
 
   /**
-   *
-   * Obtener todos los errores del CSV asociados a cada línea. OJO puede haber más de un error por línea
-   *
-   * Tip: Usar método flatmap.
-   * Tip: Usar el método extractErrors de Flight
-   *
-   */
+    *
+    * Obtener todos los errores del CSV asociados a cada línea. OJO puede haber más de un error por línea
+    *
+    * Tip: Usar método flatmap.
+    * Tip: Usar el método extractErrors de Flight
+    *
+    */
   def toErrors: RDD[(String, String)] = {
-   ???
+    val c = self.map(x=>Flight.extractErrors(x.split(","))).filter(x=>x.length>0).
+      flatMap(x=>x).map(x=>(x.hashCode.toString,x))
+    c
   }
 }
 
 class FlightFunctions(self: RDD[Flight]) {
 
   /**
-   *
-   * Obtener la distancia media recorrida por cada aeropuerto.
-   *
-   *
-   */
+    *
+    * Obtener la distancia media recorrida por cada aeropuerto.
+    *
+    *
+    */
   def averageDistanceByAirport: RDD[(String, Float)] = {
-    ???
+    val c = self.map(x=>(x.origin,x.distance))
+
+    val result = c.combineByKey(
+      (v) => (v, 1),
+      (acc: (Int, Int), v) => (acc._1 + v, acc._2 + 1),
+      (acc1: (Int, Int), acc2: (Int, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
+    ).map{ case (key, value) => (key, value._1 / value._2.toFloat) }
+
+
+    result
   }
 
   /**
-   *
-   * Obtener el consumo mínimo por aeropuerto, mes y año.
-   * @param fuelPrice RDD que contiene el precio del Fuel en un año
-   *                  y mes determinado. Ver case class {@see com.stratio.model.FuelPrice}
-   *
-   *  Tip: Primero agrupar para cada aeropuerto, mes y año y sumar las distancias de los vuelos por el precio de
-   *  combustible para ese mes y año y luego ver, para cada aeropuerto cual es el menor de los totales de los meses, año
-   *
-   *  Tip: Si el RDD es muy pequeño, podeis usar variables compartidas para evitar Joins
-   *
-   */
+    *
+    * Obtener el consumo mínimo por aeropuerto, mes y año.
+    * @param fuelPrice RDD que contiene el precio del Fuel en un año
+    *                  y mes determinado. Ver case class {@see com.stratio.model.FuelPrice}
+    *
+    *  Tip: Primero agrupar para cada aeropuerto, mes y año y sumar las distancias de los vuelos por el precio de
+    *  combustible para ese mes y año y luego ver, para cada aeropuerto cual es el menor de los totales de los meses, año
+    *
+    *  Tip: Si el RDD es muy pequeño, podeis usar variables compartidas para evitar Joins
+    *
+    */
   def minFuelConsumptionByMonthAndAirport(fuelPrice: RDD[FuelPrice]): RDD[(String, (Int, Int))] = {
-   ???
+    val c = self.map(x=>(x.origin,(1900+x.date.getYear,x.date.getMonth)))
+
+    val a = self.map(x=>((x.origin,1900+x.date.getYear,x.date.getMonth),x.distance)).reduceByKey(_+_)
+
+    val fuel =fuelPrice.cartesian(self.map(x=>x.origin)).map(x=>((x._2,x._1.year,x._1.month),x._1.price)).distinct()
+    val fuels =fuel.join(a).map(x=>(x._1,x._2._1*x._2._2))
+    val result = fuels.map(x=>(x._1._1,(x._1._2,x._1._3,x._2))).reduceByKey((x,y)=>if (x._3 < y._3) (x._1,x._2,x._3)
+    else (y._1,y._2,y._3)).map(x=>(x._1,(x._2._1,x._2._2)))
+    result
   }
 }
 
--- src/main/scala/com/stratio/model/spark/sql/FlightSqlDsl.scala
+++ src/main/scala/com/stratio/model/spark/sql/FlightSqlDsl.scala
@@ -1,54 +1,101 @@
 package com.stratio.model.spark.sql
 
+import java.sql.Date
+
+import com.stratio.model.{FlightSql, Delays, Flight}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.joda.time.LocalDate
 
 import scala.language.implicitConversions
 
 class FlightCsvReader(self: RDD[String]) {
 
   /**
-   * Creación de contexto para Spark SQL.
-   */
+    * Creación de contexto para Spark SQL.
+    */
   val sc = self.sparkContext
   val sqlContext = new SQLContext(sc)
 
   /**
-   *
-   * Parsea el RDD[String] de CSV's a un DataFrame.
-   *
-   * Tip: Crear un Flight y usar el método to FlightSql.
-   *
-   */
+    *
+    * Parsea el RDD[String] de CSV's a un DataFrame.
+    *
+    * Tip: Crear un Flight y usar el método to FlightSql.
+    *
+    */
   def toDataFrame: DataFrame = {
-    ???
+    import sqlContext.implicits._
+    val c = self.map(x=>FlightSql(Flight(
+      new LocalDate(x.split(",")(0).toInt,x.split(",")(1).toInt+1, x.split(",")(2).toInt).toDate,
+      x.split(",")(4).toInt,
+      x.split(",")(5).toInt,
+      x.split(",")(6).toInt,
+      x.split(",")(7).toInt,
+      x.split(",")(8).toString,
+      x.split(",")(9).toInt,
+      x.split(",")(11).toInt,
+      x.split(",")(12).toInt,
+      x.split(",")(14).toInt,
+      x.split(",")(15).toInt,
+      x.split(",")(16).toString,
+      x.split(",")(17).toString,
+      x.split(",")(18).toInt,
+      Flight.parseCancelled(x.split(",")(21)),
+      x.split(",")(22).toInt,
+      Delays(Flight.parseCancelled(x.split(",")(24)),
+        Flight.parseCancelled(x.split(",")(25)),
+        Flight.parseCancelled(x.split(",")(26)),
+        Flight.parseCancelled(x.split(",")(27)),
+        Flight.parseCancelled(x.split(",")(28))))))
+
+    c.toDF()
   }
 }
 
 class FlightFunctions(self: DataFrame) {
 
   /**
-   *
-   * Obtener la distancia media recorrida por cada aeropuerto.
-   *
-   * Tip: Para usar funciones de aggregación df.agg(sum('distance) as "suma"
-   *
-   */
+    *
+    * Obtener la distancia media recorrida por cada aeropuerto.
+    *
+    * Tip: Para usar funciones de aggregación df.agg(sum('distance) as "suma"
+    *
+    */
   def averageDistanceByAirport: DataFrame = {
-    ???
+    /*self.agg(sum())
+    val c = self.map(x=>(x.origin,x.distance))
+    self.agg
+    val result = c.combineByKey(
+      (v) => (v, 1),
+      (acc: (Int, Int), v) => (acc._1 + v, acc._2 + 1),
+      (acc1: (Int, Int), acc2: (Int, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
+    ).map{ case (key, value) => (key, value._1 / value._2.toFloat)
+        assert(averages(0).get(0).equals("SAN") && averages(0).get(1).equals(3926.3333333333335d))
+    assert(averages(1).get(0).equals("SFO") && averages(1).get(1).equals(330.0d))}
+    result*/
+    self.registerTempTable("flight")
+    self.sqlContext.sql("SELECT origin, AVG(distance) FROM flight group by origin").toDF()
+
   }
 
   /**
-   *
-   * Obtener el consumo mínimo por aeropuerto, mes y año.
-   * @param fuelPrice DataFrame que contiene el precio del Fuel en un año
-   *                  y mes determinado. Ver case class {@see com.stratio.model.FuelPrice}
-   *
-   *  Tip: Se pueden utilizar funciones propias del estándar SQL.
-   *  ej: Extraer el año de un campo fecha llamado date:  year('date)
-   */
+    *
+    * Obtener el consumo mínimo por aeropuerto, mes y año.
+    * @param fuelPrice DataFrame que contiene el precio del Fuel en un año
+    *                  y mes determinado. Ver case class {@see com.stratio.model.FuelPrice}
+    *
+    *  Tip: Se pueden utilizar funciones propias del estándar SQL.
+    *  ej: Extraer el año de un campo fecha llamado date:  year('date)
+    */
   def minFuelConsumptionByMonthAndAirport(fuelPrice: DataFrame): DataFrame = {
-    ???
+    self.registerTempTable("flight")
+    fuelPrice.registerTempTable("fuel")
+    val df = self.sqlContext.sql("SELECT flight.origin, year(flight.date), month(flight.date), sum(flight.distance)*fuel.price " +
+      "FROM flight JOIN fuel ON year(flight.date)=fuel.year " +
+      "AND month(flight.date)=fuel.month GROUP BY flight.origin, year(flight.date), month(flight.date), fuel.price ").toDF()
+
+    df.groupBy("origin").min("_c3").sort("min(_c3)")
   }
 
 }
@@ -62,4 +109,3 @@ trait FlightSqlDsl {
 }
 
 object FlightSqlDsl extends FlightSqlDsl
-
